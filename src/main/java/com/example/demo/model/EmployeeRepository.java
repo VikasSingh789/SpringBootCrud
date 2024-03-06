286e191b64b649a61965590af0621c939e76eb9c
Vikas Singh <59655397+VikasSingh789@users.noreply.github.com>
2024-03-06 17:49:36 +0530

    Update EmployeeRepository.java

--- src/main/java/com/example/demo/model/EmployeeRepository.java
+++ src/main/java/com/example/demo/model/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	@Query(value = "select * from employee where ename= ?1", nativeQuery = true)
+	public Employee getEmployeeByName(String ename);
 	
 }
